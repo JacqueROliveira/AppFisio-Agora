0c7ff599fcdc0a381d0e81f1070048e1e857c96a
Nathan Rocha <nathandasilva97@gmail.com>
2025-03-23 22:22:26 -0300

    Versão inicial

--- src/components/AgendamentoPrivat/index.tsx
+++ src/components/AgendamentoPrivat/index.tsx
@@ -1,4 +1,4 @@
-import {Container, SubTitle, TextSelecao, CadastroLogo,  FixaCadastro, Logo, RetornoDados, Container1, Title, Position, Preco, Value} from './style'
+import {Container, SubTitle, TextSelecao, CadastroLogo, Logo, Title, Position, Preco, Value} from './style'
 import logoimg from 'assets/Logo-letra.png'
 import { ButtonCadastro } from '@components/Button';
 import ImputDataPrivat from '@components/imputDataPrivat';
